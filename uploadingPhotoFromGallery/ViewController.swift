19219b858b0724091c88aa55a158dd3cd35dc6fa
ömer faruk coşar <ofcosar1@gmail.com>
2022-11-21 13:45:37 +0300

    done commit

--- uploadingPhotoFromGallery/ViewController.swift
+++ uploadingPhotoFromGallery/ViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var photoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //test
+        print("pick Act")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickAction(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+}
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage{
+            photoView.image = image
+        }
+        picker.dismiss(animated: true)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
 }
 
